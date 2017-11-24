02ff5bfd23dbd891578f28774c09e4b951fe42ae
Josep PINYOT <jpinyot@e3r4p2.42.fr>
2017-11-24 20:45:06 +0100

    bigmom modifications

---
+++ ft_big_mom.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_big_mom.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzabalza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/24 01:24:57 by mzabalza          #+#    #+#             */
+/*   Updated: 2017/11/24 20:42:07 by jpinyot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libtet.h"
+#include "libft.h"
+
+void	ft_big_mom(t_piece *list, char *patern)
+{
+	while (list)
+	{
+		if (!(ft_checkfree(patern)))
+			ft_patern_add(patern);
+		if (!(ft_add_piece(patern, list, ft_currentcord(patern, list))))
+		{
+			if (list->prev)
+				list = list->prev;
+			else
+				ft_patern_add(patern);
+		}
+		else
+			list = list->next;
+	}
+	ft_putstr(patern);
+}
